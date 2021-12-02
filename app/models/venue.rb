4c24cc09bf172227d452f7fb4e838b9548aef891
Vivien Timmers <djyak.be@gmail.com>
2021-12-02 16:45:13 +1100

    init

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,14 @@ class Venue < ApplicationRecord
   belongs_to :user
   has_many :spaces, dependent: :destroy
   has_many :bookings, through: :spaces
+
+  geocoded_by :address do |obj,results|
+    if geo = results.first
+      obj.latitude    = geo.latitude
+      obj.longitude    = geo.longitude
+    end
+  end
+
+  after_validation :geocode
+
 end
