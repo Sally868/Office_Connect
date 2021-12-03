0314724eedccd944637aba5b85b9042910fcb49d
swetha pattanshetty shivakumar <swetha.ps12@gmail.com>
2021-12-03 15:16:50 +1100

    Adding delete feature

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,4 +1,5 @@
 class SpacesController < ApplicationController
+   before_action :set_space, only: %i[show edit update destroy]
    def index
     @venue = Venue.find(params[:venue_id])
     @spaces = Space.all
@@ -39,12 +40,12 @@ class SpacesController < ApplicationController
   def destroy
     # authorize if owner -> yes. else no
     @space.destroy
-    redirect_to spaces_path
+    redirect_to venue_path(@space.venue)
   end
 
   private
 
-  def set_venue
+  def set_space
     @space = Space.find(params[:id])
 
   end
