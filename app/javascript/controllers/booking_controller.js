578d3f64f4b371fff95cfb62b2e847765e0df4dc
Sally868 <kimdungqcd1@gmail.com>
2021-12-07 15:42:32 +1030

    check time zone

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -30,17 +30,17 @@ export default class extends Controller {
         this.selectedStarttime = selectedDatetime
         this.clearAllSelected()
         // this.startTarget.value = this.selectedStarttime
-        this.startTarget.value = window.moment(selectedDatetime).utc().format();
+        this.startTarget.value = window.moment(selectedDatetime).format();
 
         clickedElement.classList.add('bg-green-500')
         // find startInputTarget and set value to data-datetime of the element that was just clicked
     } else {
         // find the endInputTarget and set value to data-datetime of the elemnt that was just clicked
-        const dateLarger = window.moment(selectedDatetime).utc().isSameOrAfter(window.moment(this.selectedStarttime));
+        const dateLarger = window.moment(selectedDatetime).isSameOrAfter(window.moment(this.selectedStarttime));
         if (dateLarger) {
             this.selectedFinishtime = selectedDatetime
             console.log(selectedDatetime)
-            this.finishTarget.value = window.moment(selectedDatetime).utc().add(60, 'm').format();
+            this.finishTarget.value = window.moment(selectedDatetime).add(60, 'm').format();
             this.selectInBetween()
             clickedElement.classList.add('bg-green-500')
         } else {
